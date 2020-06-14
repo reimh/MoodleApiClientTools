0d36fb4fb170bd66f9fdc7f5878ca55f80c3db80
reimh <reimers@heye-tammo.de>
2020-06-14 20:42:12 +0200

    add example

---
+++ example/010-List_all_users/index.php
@@ -0,0 +1,47 @@
+<?php
+	echo "<h1> Testing 'MoodleApiClientTools' </h1>";
+
+	$filename= file_exists("settings.ini.php") ? "settings.ini.php" : "settings.ini.samle.php";
+	echo "Try settingsfile '$filename'.<br>\n";
+	$settings=parse_ini_file($filename,true);
+	
+	if( !isset($settings["moodle"]["url"]) OR !isset($settings["moodle"]["token"]) ){
+                echo "url or token missing";
+                exit(0);
+        }
+
+        $url=$settings["moodle"]["url"];
+        $token=$settings["moodle"]["token"];
+
+
+	//A simple Moodle API test. Requires access to the functions 'core_webservice_get_site_info' and 'core_user_get_users_by_field'
+	// 1. Try to get API-Information form moodle
+	// 2. Try to get information from an user
+
+	$loginname = isset($_GET['loginname']) ? trim($_GET['loginname']) : 'try_a_loginname';
+	echo "
+		<form action='' method='get' id='testapi'>
+			URL: <input type='text' name='url' size='40' value='$url'><br>
+			Token: <input type='text' name='token' size='40'   value='$token'><br>
+			Search loginname: <input type='text' name='loginname' size='40'   value='$loginname'><br>
+		</form>
+
+		<button type='submit' form='testapi' value='testapi'>Test API</button><br><br>";
+
+	(@include_once ('../../src/MoodleApiClient.php')) OR die("Lib 'MoodleApiClient.php' not found. Download file and copy it to this folder!");
+
+	
+	//Try to connect an getting data
+	echo "Try connecting to '$url'.<br>\n"; 
+	$MoodleApiClient = new MoodleApiClient($url, $token);
+		
+	//Try to get userdata
+	$params= array("field"=>"username", "values"=> array( "$loginname") );
+	$responce=$MoodleApiClient->sendRequest("core_user_get_users_by_field",$params);
+
+	echo "Last API-Request url: '".str_replace($token,"(secret_token)",$MoodleApiClient->getLastUrl())."'<br>\n";
+	echo "<PRE>";
+		print_r($responce);
+	echo "</PRE>";			
+			
+?>
---
+++ example/010-List_all_users/settings.ini.sample.php
@@ -0,0 +1,5 @@
+;<?php die(); ?>
+
+[moodle]
+url = "https://moodle.mydomain.org/webservice/rest/server.php"
+token ="kgju66bc8754bjf43bghjr784bf"
